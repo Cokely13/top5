dbb194282b265f608af023823bceec5b908c107a
Ryan Cokely <ryan.cokely@gmail.com>
2024-10-10 18:34:34 -0400

    fixing store

--- client/store/index.js
+++ client/store/index.js
@@ -7,38 +7,13 @@ import usersReducer from './allUsersStore'
 import singleUserReducer from './singleUserStore'
 import singleQuestionReducer from './singleQuestionStore'
 import questionsReducer from './allQuestionsStore'
-import groupQuestionsReducer from './allGroupQuestionStore'
-import singleGroupReducer from './singleGroupStore'
-import groupsReducer from './allGroupsStore'
-import consensusesReducer from './allConsensusesStore'
-import singleConsensusReducer from './singleConsensusStore'
-import groupMembersReducer from './allGroupMembersStore'
-import singleGroupMemberReducer from './singleGroupMemberStore'
-import singleGroupQuestionReducer from './singleGroupQuestionStore'
-import singleUserResponseReducer from './singleUserResponseStore'
-import userResponsesReducer from './allUserResponsesStore'
-import singleInviteReducer from './singleInviteStore'
-import invitesReducer from './allInvitesStore'
-import messagesReducer from './allMessagesStore'
+
 
 const reducer = combineReducers({ auth,
   allUsers: usersReducer,
   allQuestions: questionsReducer,
   singleQuestion: singleQuestionReducer,
   singleUser: singleUserReducer,
-  singleGroup: singleGroupReducer,
-  allGroups: groupsReducer,
-  singleConsensus: singleConsensusReducer,
-  allConsensuses: consensusesReducer,
-  singleGroupMember: singleGroupMemberReducer,
-  allGroupMembers: groupMembersReducer,
-  singleGroupQuestion: singleGroupQuestionReducer,
-  allGroupQuestions: groupQuestionsReducer,
-  singleUserResponse: singleUserResponseReducer,
-  allUserResponses: userResponsesReducer,
-  singleInvite: singleInviteReducer,
-  allInvites: invitesReducer,
-  allMessages: messagesReducer
  })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
